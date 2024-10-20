e43c1912c2e1e23f975678b92c9bdd1525220724
Egge <egge@getcurrent.io>
2024-10-20 06:50:51 +0200

    design: updated navbar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import { useAppSelector } from "./store/store";
+import { LuTreePine } from "react-icons/lu";
 
 function NavBar() {
   const basketSize = useAppSelector((state) => state.basket.items.length);
   return (
-    <div className="w-full bg-zinc-100 border-black border-b-[1px] flex items-center justify-between p-2">
-      <p className="font-bold text-xl">TINYPINE</p>
+    <div className="w-full bg-zinc-900 text-zinc-100 border-black border-b-[1px] flex items-center justify-between p-2">
+      <div className="flex items-center gap-1">
+        <LuTreePine className="text-xl" />
+        <p className="font-bold text-xl">TINYPINE</p>
+      </div>
       <div className="flex gap-2 items-center">
         <Link to="/">Items</Link>
         <Link to={"/basket"} className="flex gap-1 rounded px-2 py-1">
-          <p className="px-2 py-1 border-black border-[1px] rounded">Basket</p>
+          <p className="px-2 py-1 border-zinc-100 border-[1px] rounded">
+            Basket
+          </p>
           {basketSize > 0 ? (
             <p className="bg-orange-300 px-2 py-1 rounded ">{basketSize}</p>
           ) : undefined}
