0f1239f7c49d1bced03fc6a629d7d3bfa02ce73b
Egge <egge@getcurrent.io>
2024-10-25 05:35:21 +0100

    gen: added logout

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,28 +1,27 @@
-import { Link } from "react-router-dom";
-import { useAppSelector } from "./store/store";
+import { Link, useNavigate } from "react-router-dom";
 import { LuTreePine } from "react-icons/lu";
 
 function NavBar() {
-  const basketSize = useAppSelector((state) => state.basket.items.length);
+  const navigate = useNavigate();
   return (
     <div className="w-full bg-zinc-900 text-zinc-100 border-black border-b-[1px] flex items-center justify-between p-2">
       <div className="flex items-center gap-1">
         <LuTreePine className="text-xl" />
         <p className="font-bold text-xl">TINYPINE</p>
       </div>
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-4 items-center">
         <Link to="/wallet">Wallet</Link>
         <Link to="/">Items</Link>
         <Link to="/orders">Orders</Link>
         <Link to="/settings">Settings</Link>
-        <Link to={"/basket"} className="flex gap-1 rounded px-2 py-1">
-          <p className="px-2 py-1 border-zinc-100 border-[1px] rounded">
-            Basket
-          </p>
-          {basketSize > 0 ? (
-            <p className="bg-orange-300 px-2 py-1 rounded ">{basketSize}</p>
-          ) : undefined}
-        </Link>
+        <button
+          onClick={() => {
+            localStorage.clear();
+            navigate(0);
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
